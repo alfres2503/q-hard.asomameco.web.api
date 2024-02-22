e5e382782879b155c579c5b471cb2a7544c612a4
Alfredo Suárez <94250490+alfres2503@users.noreply.github.com>
2024-02-22 12:21:34 -0600

    test: structure, refactor & add ControllersTests

---
+++ test/Controllers/MembersControllerTests.cs
@@ -0,0 +1,241 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using src.Controllers;
+using src.Models;
+using src.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test.Controllers
+{
+    public class MembersControllerTests
+    {
+        private readonly IFixture _fixture;
+        private readonly Mock<IMemberService> _serviceMock;
+        private readonly MembersController _controller;
+
+        public MembersControllerTests()
+        {
+            _fixture = new Fixture();
+            _serviceMock = _fixture.Freeze<Mock<IMemberService>>();
+            _controller = new MembersController(_serviceMock.Object);
+        }
+
+        // Test Get All
+        [Fact]
+        public async Task GetAll_ShouldReturnOkResponse_WhenDataFound()
+        {
+            // Arrange
+            var membersMock = _fixture.CreateMany<Member>(3).ToList();
+
+            _serviceMock.Setup(service => service.GetAll()).ReturnsAsync(membersMock);
+
+            // Act
+            var result = await _controller.GetMembers().ConfigureAwait(false);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeAssignableTo<ActionResult<IEnumerable<Member>>>();
+            result.Result.Should().BeOfType<OkObjectResult>();
+            result.Result.As<OkObjectResult>().Value
+                .Should()
+                .NotBeNull()
+                .And.BeOfType(membersMock.GetType());
+            _serviceMock.Verify(service => service.GetAll(), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetAll_ShouldReturnNoContent_WhenNoDataFound()
+        {
+            // Arrange
+            _serviceMock.Setup(service => service.GetAll()).ReturnsAsync((List<Member>)null);
+
+            // Act
+            var result = await _controller.GetMembers().ConfigureAwait(false);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Result.Should().BeOfType<NoContentResult>();
+            _serviceMock.Verify(service => service.GetAll(), Times.Once);
+        }
+
+
+        [Fact]
+        public async Task GetAll_ShouldReturnInternalServerError_WhenExceptionThrown()
+        {
+            // Arrange
+            _serviceMock.Setup(service => service.GetAll()).ThrowsAsync(new Exception("Some error"));
+
+            // Act
+            var result = await _controller.GetMembers().ConfigureAwait(false);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Result.Should().BeOfType<ObjectResult>();
+            result.Result.As<ObjectResult>().StatusCode.Should().Be(StatusCodes.Status500InternalServerError);
+            _serviceMock.Verify(service => service.GetAll(), Times.Once);
+        }
+
+        // Test Get By ID
+        [Fact]
+        public async Task GetByID_ShouldReturnOkResponse_WhenValidInput()
+        {
+            // Arrange
+            var memberMock = _fixture.Create<Member>();
+            var id = _fixture.Create<int>();
+            _serviceMock.Setup(service => service.GetByID(id)).ReturnsAsync(memberMock);
+
+            // Act
+            var result = await _controller.GetMemberById(id).ConfigureAwait(false);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeAssignableTo<ActionResult<Member>>();
+            result.Result.Should().BeOfType<OkObjectResult>();
+            result.Result.As<OkObjectResult>().Value
+                .Should()
+                .NotBeNull()
+                .And.BeOfType(memberMock.GetType());
+            _serviceMock.Verify(service => service.GetByID(id), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetByID_ShouldReturnNoContent_WhenNoDataFound()
+        {
+            // Arrange
+            Member response = null;
+            var id = _fixture.Create<int>();
+            _serviceMock.Setup(service => service.GetByID(id)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetMemberById(id).ConfigureAwait(false);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Result.Should().BeOfType<NoContentResult>();
+            _serviceMock.Verify(service => service.GetByID(id), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetByID_ShouldReturnBadRequest_WhenInputIsZero()
+        {
+            // Arrange
+            var id = 0;
+
+            // Act
+            var result = await _controller.GetMemberById(id).ConfigureAwait(false);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Result.Should().BeOfType<BadRequestObjectResult>();
+            _serviceMock.Verify(service => service.GetByID(id), Times.Never);
+        }
+
+        [Fact]
+        public async Task GetByID_ShouldReturnBadRequest_WhenInputIsLessThanZero()
+        {
+            // Arrange
+            var id = -1;
+
+            // Act
+            var result = await _controller.GetMemberById(id).ConfigureAwait(false);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Result.Should().BeOfType<BadRequestObjectResult>();
+            _serviceMock.Verify(service => service.GetByID(id), Times.Never);
+        }
+
+        //test exception
+        [Fact]
+        public async Task GetByID_ShouldReturnInternalServerError_WhenExceptionThrown()
+        {
+            // Arrange
+            var id = _fixture.Create<int>();
+            _serviceMock.Setup(service => service.GetByID(id)).ThrowsAsync(new Exception("Some error"));
+
+            // Act
+            var result = await _controller.GetMemberById(id).ConfigureAwait(false);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Result.Should().BeOfType<ObjectResult>();
+            result.Result.As<ObjectResult>().StatusCode.Should().Be(StatusCodes.Status500InternalServerError);
+            _serviceMock.Verify(service => service.GetByID(id), Times.Once);
+        }
+
+        // Test Create
+        [Fact]
+        public async Task Create_ShouldReturnCreatedResponse_WhenValidInput()
+        {
+            // Arrange
+            var request = _fixture.Create<Member>();
+            var response = _fixture.Create<Member>();
+            _serviceMock.Setup(service => service.Create(request)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.CreateMember(request).ConfigureAwait(false);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeAssignableTo<ActionResult<Member>>();
+            result.Result.Should().BeOfType<CreatedAtRouteResult>();
+
+            _serviceMock.Verify(service => service.Create(request), Times.Once);    
+        }
+
+        [Fact]
+        public async Task Create_ShouldReturnBadRequest_WhenModelStateIsInvalid()
+        {
+            // Arrange
+            var request = _fixture.Create<Member>();
+            _controller.ModelState.AddModelError("Name", "Name is required");
+
+            // Act
+            var result = await _controller.CreateMember(request).ConfigureAwait(false);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Result.Should().BeOfType<BadRequestObjectResult>();
+            _serviceMock.Verify(service => service.Create(request), Times.Never);
+        }
+
+        [Fact]
+        public async Task Create_ShouldReturnBadRequest_WhenInputIsNull()
+        {
+            // Arrange
+            Member request = null;
+
+            // Act
+            var result = await _controller.CreateMember(request).ConfigureAwait(false);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Result.Should().BeOfType<BadRequestObjectResult>();
+            _serviceMock.Verify(service => service.Create(request), Times.Never);
+        }
+
+        [Fact]
+        public async Task Create_ShouldReturnInternalServerError_WhenExceptionThrown()
+        {
+            // Arrange
+            var request = _fixture.Create<Member>();
+            _serviceMock.Setup(service => service.Create(request)).ThrowsAsync(new Exception("Some error"));
+
+            // Act
+            var result = await _controller.CreateMember(request).ConfigureAwait(false);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Result.Should().BeOfType<ObjectResult>();
+            result.Result.As<ObjectResult>().StatusCode.Should().Be(StatusCodes.Status500InternalServerError);
+            _serviceMock.Verify(service => service.Create(request), Times.Once);
+        }
+    }
+}
---
+++ test/Services/MemberServiceTests.cs
@@ -0,0 +1,234 @@
+using AutoFixture;
+using FluentAssertions;
+using Moq;
+using src.Models;
+using src.Repository;
+using src.Services;
+using src.Utils;
+
+namespace test.Services
+{
+    public class MemberServiceTests
+    {
+        private readonly IFixture _fixture;
+        private readonly IMemberService _service;
+        private readonly Mock<IMemberRepository> _repositoryMock;
+
+        public MemberServiceTests()
+        {
+            _fixture = new Fixture();
+            _repositoryMock = _fixture.Freeze<Mock<IMemberRepository>>();
+            _service = new MemberService(_repositoryMock.Object);
+        }
+
+        // Test Get All
+        [Fact]
+        public async Task GetAll_ShouldReturnData_WhenDataFound()
+        {
+            // Arrange
+            var membersMock = _fixture.CreateMany<Member>(3).ToList();
+
+            _repositoryMock.Setup(repo => repo.GetAll()).ReturnsAsync(membersMock);
+
+            // Act
+            var result = await _service.GetAll().ConfigureAwait(false);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeAssignableTo<IEnumerable<Member>>();
+            result.Should().BeEquivalentTo(membersMock);
+            _repositoryMock.Verify(repo => repo.GetAll(), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetAll_ShouldReturnEmpty_WhenNoDataFound()
+        {
+            // Arrange
+            _repositoryMock.Setup(repo => repo.GetAll()).ReturnsAsync(Enumerable.Empty<Member>());
+
+            // Act
+            var result = await _service.GetAll().ConfigureAwait(false);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeAssignableTo<IEnumerable<Member>>();
+            result.Should().BeEmpty();
+            _repositoryMock.Verify(repo => repo.GetAll(), Times.Once);
+        }
+
+        // Test Get By Email
+        [Fact]
+        public async Task GetByEmail_ShouldReturnData_WhenDataFound()
+        {
+            // Arrange
+            var memberMock = _fixture.Create<Member>();
+
+            _repositoryMock.Setup(repo => repo.GetByEmail(memberMock.Email)).ReturnsAsync(memberMock);
+
+            // Act
+            var result = await _service.GetByEmail(memberMock.Email).ConfigureAwait(false);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeAssignableTo<Member>();
+            result.Should().BeEquivalentTo(memberMock);
+            _repositoryMock.Verify(repo => repo.GetByEmail(memberMock.Email), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetByEmail_ShouldReturnNull_WhenNoDataFound()
+        {
+            // Arrange
+            var memberMock = _fixture.Create<Member>();
+
+            _repositoryMock.Setup(repo => repo.GetByEmail(memberMock.Email)).ReturnsAsync((Member)null);
+
+            // Act
+            var result = await _service.GetByEmail(memberMock.Email).ConfigureAwait(false);
+
+            // Assert
+            result.Should().BeNull();
+            _repositoryMock.Verify(repo => repo.GetByEmail(memberMock.Email), Times.Once);
+        }
+
+        // Test Get By ID
+        [Fact]
+        public async Task GetByID_ShouldReturnData_WhenDataFound()
+        {
+            // Arrange
+            var memberMock = _fixture.Create<Member>();
+
+            _repositoryMock.Setup(repo => repo.GetByID(memberMock.Id)).ReturnsAsync(memberMock);
+
+            // Act
+            var result = await _service.GetByID(memberMock.Id).ConfigureAwait(false);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeAssignableTo<Member>();
+            result.Should().BeEquivalentTo(memberMock);
+            _repositoryMock.Verify(repo => repo.GetByID(memberMock.Id), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetByID_ShouldReturnNull_WhenNoDataFound()
+        {
+            // Arrange
+            var memberMock = _fixture.Create<Member>();
+
+            _repositoryMock.Setup(repo => repo.GetByID(memberMock.Id)).ReturnsAsync((Member)null);
+
+            // Act
+            var result = await _service.GetByID(memberMock.Id).ConfigureAwait(false);
+
+            // Assert
+            result.Should().BeNull();
+            _repositoryMock.Verify(repo => repo.GetByID(memberMock.Id), Times.Once);
+        }
+
+        // Test Create
+        [Fact]
+        public async Task Create_ShouldReturnData_WhenDataCreated()
+        {
+            // Arrange
+            var memberMock = _fixture.Create<Member>();
+
+            _repositoryMock.Setup(repo => repo.Create(memberMock)).ReturnsAsync(memberMock);
+
+            // Act
+            var result = await _service.Create(memberMock).ConfigureAwait(false);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeAssignableTo<Member>();
+            result.Should().BeEquivalentTo(memberMock);
+            _repositoryMock.Verify(repo => repo.Create(memberMock), Times.Once);
+        }
+
+        [Fact]
+        public async Task Create_ShouldReturnNull_WhenDataNotCreated()
+        {
+            // Arrange
+            var memberMock = _fixture.Create<Member>();
+
+            _repositoryMock.Setup(repo => repo.Create(memberMock)).ReturnsAsync((Member)null);
+
+            // Act
+            var result = await _service.Create(memberMock).ConfigureAwait(false);
+
+            // Assert
+            result.Should().BeNull();
+            _repositoryMock.Verify(repo => repo.Create(memberMock), Times.Once);
+        }
+
+        // Test Delete 
+        [Fact]
+        public async Task Delete_ShouldReturnTrue_WhenDataDeleted()
+        {
+            // Arrange
+            var memberMock = _fixture.Create<Member>();
+            _repositoryMock.Setup(repo => repo.Delete(memberMock.Id)).ReturnsAsync(true);
+
+            // Act
+            var result = await _service.Delete(memberMock.Id).ConfigureAwait(false);
+
+            // Assert
+            result.Should().BeTrue();
+            _repositoryMock.Verify(repo => repo.Delete(memberMock.Id), Times.Once);
+        }
+
+        [Fact]
+        public async Task Delete_ShouldReturnFalse_WhenDataNotDeleted()
+        {
+            // Arrange
+            var memberMock = _fixture.Create<Member>();
+
+            _repositoryMock.Setup(repo => repo.Delete(memberMock.Id)).ReturnsAsync(false);
+
+            // Act
+            var result = await _service.Delete(memberMock.Id).ConfigureAwait(false);
+
+            // Assert
+            result.Should().BeFalse();
+            _repositoryMock.Verify(repo => repo.Delete(memberMock.Id), Times.Once);
+        }
+
+        // Test Update
+        [Fact]
+        public async Task Update_ShouldReturnUpdatedData_WhenDataUpdated()
+        {
+            // Arrange
+            var memberMock = _fixture.Create<Member>();
+            var response = _fixture.Create<Member>();
+
+            _repositoryMock.Setup(repo => repo.Update(memberMock)).ReturnsAsync(response);
+
+            // Act
+            var result = await _service.Update(memberMock).ConfigureAwait(false);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeAssignableTo<Member>();
+            result.Should().BeEquivalentTo(response);
+            _repositoryMock.Verify(repo => repo.Update(memberMock), Times.Once);
+        }
+
+        [Fact]
+        public async Task Update_ShouldReturnNull_WhenDataNotUpdated()
+        {
+            // Arrange
+            var memberMock = _fixture.Create<Member>();
+
+            _repositoryMock.Setup(repo => repo.Update(memberMock)).ReturnsAsync((Member)null);
+
+            // Act
+            var result = await _service.Update(memberMock).ConfigureAwait(false);
+
+            // Assert
+            result.Should().BeNull();
+            _repositoryMock.Verify(repo => repo.Update(memberMock), Times.Once);
+        }
+
+
+    }
+}
