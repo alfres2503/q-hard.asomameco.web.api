0ba68d50afd457aa82bcb5cf446a2e947ca4f7b1
Gabulate <gabrielulate71@gmail.com>
2024-02-28 13:56:33 -0600

    feat: Event Repository GetAll, GetByID, Create, Update, Delete Methods

--- src/Repository/EventRepository.cs
+++ src/Repository/EventRepository.cs
@@ -1,32 +1,143 @@
-﻿using src.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using src.Models;
 
 namespace src.Repository
 {
     public class EventRepository : IEventRepository
     {
-        public Task<Event> Create(Event _event)
+        private readonly AppDBContext _context;
+
+        public EventRepository(AppDBContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public Task<bool> Delete(int id)
+        // async method to get all events
+        public async Task<Event> Create(Event _event)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await _context.Event.AddAsync(_event);
+                await _context.SaveChangesAsync();
+
+                if (this.GetByID(_event.Id) != null)
+                    return _event;
+                else
+                    throw new Exception("Event not created");
+            }
+            catch (DbUpdateException dbEx)
+            {
+                throw new Exception($"Database error: {dbEx.Message}", dbEx);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred: {ex.Message}", ex);
+            }
         }
 
-        public Task<IEnumerable<Event>> GetAll(int pageNumber, int pageSize)
+        public async Task<bool> Delete(int id)
         {
-            throw new NotImplementedException();
+            bool deletedStatus = false;
+            try
+            {
+                Event _event = await _context.Event.FindAsync(id);
+
+                if (_event == null)
+                    return deletedStatus;
+
+                _context.Event.Remove(_event);
+                await _context.SaveChangesAsync();
+
+                _event = await _context.Event.FindAsync(id);
+                if (_event == null)
+                    deletedStatus = true;
+
+                return deletedStatus;
+            }
+            catch (DbUpdateException dbEx)
+            {
+                throw new Exception($"Database error: {dbEx.Message}", dbEx);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred: {ex.Message}", ex);
+            }
         }
 
-        public Task<Event> GetByID(int id)
+        public async Task<IEnumerable<Event>> GetAll(int pageNumber, int pageSize)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return await _context.Event
+                    .OrderBy(e => e.Date)
+                    .Skip((pageNumber - 1) * pageSize)
+                    .Take(pageSize)
+                    .Select(e => new Event
+                    {
+                        Id = e.Id,
+                        IdMember = e.IdMember,
+                        Name = e.Name,
+                        Description = e.Description,
+                        Date = e.Date,
+                        Place = e.Place,
+                        Time = e.Time
+                    })
+                    .ToListAsync();
+            }
+            catch (DbUpdateException dbEx)
+            {
+                throw new Exception($"Database error: {dbEx.Message}", dbEx);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred: {ex.Message}", ex);
+            }
         }
 
-        public Task<Event> Update(Event _event)
+        public async Task<Event> GetByID(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return await _context.Event
+                    .Include(e => e.Member)
+                    .Select(e => new Event
+                    {
+                        Id = e.Id,
+                        IdMember = e.IdMember,
+                        Name = e.Name,
+                        Description = e.Description,
+                        Date = e.Date,
+                        Place = e.Place,
+                        Time = e.Time
+                    })
+                    .FirstOrDefaultAsync(e => e.Id == id);
+            }
+            catch (DbUpdateException dbEx)
+            {
+                throw new Exception($"Database error: {dbEx.Message}", dbEx);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred: {ex.Message}", ex);
+            }
+        }
+
+        public async Task<Event> Update(Event _event)
+        {
+            try
+            {
+                _context.Entry(_event).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return _event;
+            }
+            catch (DbUpdateException dbEx)
+            {
+                throw new Exception($"Database error: {dbEx.Message}", dbEx);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred: {ex.Message}", ex);
+            }
         }
     }
 }
