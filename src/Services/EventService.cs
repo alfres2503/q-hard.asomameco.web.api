66d2d965bab8f51d8326c7a4c7a8c4c6c9830ad5
Gabulate <gabrielulate71@gmail.com>
2024-02-28 13:44:03 -0600

    feat: Event Service class

---
+++ src/Services/EventService.cs
@@ -0,0 +1,32 @@
+﻿using src.Models;
+
+namespace src.Services
+{
+    public class EventService : IEventService
+    {
+        public Task<Event> Create(Event _event)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Event>> GetAll(int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Event> GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Event> Update(Event _event)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
