2ed7921f0253b3f9c51364441f3902438f81f9b1
Gabulate <gabrielulate71@gmail.com>
2024-02-28 13:43:04 -0600

    feat: Event Reposirory class

---
+++ src/Repository/EventRepository.cs
@@ -0,0 +1,32 @@
+﻿using src.Models;
+
+namespace src.Repository
+{
+    public class EventRepository : IEventRepository
+    {
+        public Task<Event> Create(Event _event)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Event>> GetAll(int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Event> GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Event> Update(Event _event)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
