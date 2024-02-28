db81863f35a2c33cf2be6e15ec0c18a896a723aa
Gabulate <gabrielulate71@gmail.com>
2024-02-28 13:40:58 -0600

    feat: Event Repository Interface

---
+++ src/Repository/IEventRepository.cs
@@ -0,0 +1,13 @@
+﻿using src.Models;
+
+namespace src.Repository
+{
+    public interface IEventRepository
+    {
+        Task<IEnumerable<Event>> GetAll(int pageNumber, int pageSize);
+        Task<Event> GetByID(int id);
+        Task<Event> Create(Event _event);
+        Task<Event> Update(Event _event);
+        Task<bool> Delete(int id);
+    }
+}
