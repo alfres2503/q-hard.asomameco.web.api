d48ca4981751e99e1d766aa3cb54b28cc7bd73b1
fioresf <fiorellasalasfonseca670@gmail.com>
2024-03-09 13:01:09 -0600

    feat: Role Service class

---
+++ src/Services/RoleService.cs
@@ -0,0 +1,32 @@
+﻿using src.Models;
+
+namespace src.Services
+{
+    public class RoleService : IRoleService
+    {
+        public Task<Role> Create(Role role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Role>> GetAll(int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Role> GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Role> Update(Role role)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
