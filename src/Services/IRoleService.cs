13228deb51bea575f41b9a1441a3e9789d05b1bc
fioresf <fiorellasalasfonseca670@gmail.com>
2024-03-09 12:57:24 -0600

    feat: Role Sevice Interface

---
+++ src/Services/IRoleService.cs
@@ -0,0 +1,13 @@
+﻿using src.Models;
+
+namespace src.Services
+{
+    public interface IRoleService
+    {
+        Task<IEnumerable<Role>> GetAll(int pageNumber, int pageSize);
+        Task<Role> GetByID(int id);
+        Task<Role> Create(Role role);
+        Task<Role> Update(Role role);
+        Task<bool> Delete(int id);
+    }
+}
