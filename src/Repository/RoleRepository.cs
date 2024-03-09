664e0106fe7a2578c111e69436b31d2ae7e48060
fioresf <fiorellasalasfonseca670@gmail.com>
2024-03-09 13:00:02 -0600

    feat: Role Repository class

---
+++ src/Repository/RoleRepository.cs
@@ -0,0 +1,32 @@
+﻿using src.Models;
+
+namespace src.Repository
+{
+    public class RoleRepository : IRoleRepository
+    {
+        public Task<Role> Create(Role role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Role>> GetAll(int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Role> GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Role> Update(Role role)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
