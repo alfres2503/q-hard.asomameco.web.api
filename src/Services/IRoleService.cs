daee2332930028140d3b4e81fb6af7bc92d88298
fioresf <fiorellasalasfonseca670@gmail.com>
2024-03-17 13:02:29 -0600

    feat: GetCount method in IRoleService.

--- src/Services/IRoleService.cs
+++ src/Services/IRoleService.cs
@@ -5,6 +5,7 @@ namespace src.Services
     public interface IRoleService
     {
         Task<IEnumerable<Role>> GetAll(int pageNumber, int pageSize);
+        Task<int> GetCount();
         Task<Role> GetByID(int id);
         Task<Role> Create(Role role);
         Task<Role> Update(Role role);
