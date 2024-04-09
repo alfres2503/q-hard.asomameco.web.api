24236639c335aed60cd193dbbed139ef8c98b6be
fioresf <fiorellasalasfonseca670@gmail.com>
2024-04-09 16:29:02 -0600

    fead: Updated IAttendanceRepository methods.

--- src/Repository/IAttendanceRepository.cs
+++ src/Repository/IAttendanceRepository.cs
@@ -9,11 +9,13 @@ namespace src.Repository
 {
     public interface IAttendanceRepository
     {
-        Task<IEnumerable<Attendance>> GetAll(int pageNumber, int pageSize);
-        Task<int> GetCount();
-        Task<IEnumerable<Attendance>> GetByIdEvent(int id,int pageNumber, int pageSize);
-        Task<IEnumerable<Attendance>> GetByIdAssociate(int id, int pageNumber, int pageSize);
+        Task<IEnumerable<Attendance>> GetAll(int pageNumber, int pageSize, string searchTerm, string orderBy);
+        Task<int> GetCount(string searchTerm);
+        Task<IEnumerable<Attendance>> GetByIdEvent(int id,int pageNumber, int pageSize, string searchTerm, string orderBy);
+        Task<IEnumerable<Attendance>> GetByIdAssociate(int id, int pageNumber, int pageSize, string searchTerm, string orderBy);
         Task<Attendance> GetByIdEventIdAssociate(int idEvent, int idAssociate);
         Task<Attendance> Create(Attendance attendance);
+        Task<Attendance> Update(int idEvent,int idAssociate, Attendance attendance);
+        Task<Attendance> ChangeState(int idEvent, int idAssociate);
     }
 }
