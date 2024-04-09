7b35341225fdd9d7d6e49fd5de366d6b50871e93
fioresf <fiorellasalasfonseca670@gmail.com>
2024-04-09 16:59:30 -0600

    feat: Update AttendanceService method.

--- src/Services/IAttendanceService.cs
+++ src/Services/IAttendanceService.cs
@@ -5,7 +5,7 @@ namespace src.Services
     public interface IAttendanceService
     {
         Task<IEnumerable<Attendance>> GetAll(int pageNumber, int pageSize, string searchTerm, string orderBy);
-        Task<int> GetCount();
+        Task<int> GetCount(string searchTerm);
         Task<IEnumerable<Attendance>> GetByIdEvent(int id, int pageNumber, int pageSize);
         Task<IEnumerable<Attendance>> GetByIdAssociate(int id, int pageNumber, int pageSize);
         Task<Attendance> GetByIdEventIdAssociate(int idEvent, int idAssociate);
